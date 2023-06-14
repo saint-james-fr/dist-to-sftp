e970cbf56f2a76b2fe1ffd7b391077daae5009f6
Maxence Robinet <maxencerobinet@gmail.com>
2023-06-14 13:27:47 +0200

    logger fixed again

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,19 @@
 export const logger = {
-  // Errors
+  // CLI Errors
   stopping: "Stopping execution...",
   missingValueForOption: (option) =>
     `⚠️    Missing value for option: ${option}`,
   unknownOption: (option) => `⚠️    Unknown option: ${option}`,
+  // SFTP errors
   missingCredentials: "\n❌    Some credentials are missing.\n",
   missingDistFolder:
     "\n❌    No dist directory found. Please make sure you provided correct path from root of your project. \nEx './dist'.\n",
   sftpError: (err) => `\n❌    SFTP error: ${err}\n`,
+  // Options Errors
   missingEnvVariables: "\n⚠️    Some variables are missing in the .env file.\n",
   missingEnvFile:"\n⚠️    No .env file found. Creating a new one.\n",
   missingGitRepository: "⚠️    No Git repository found.\nMake sure you initialize a Git repository and add .env to your .gitignore file.\n",
+  // Files Errors
   cleanDirectoryError: (err) => `\n❌    Error cleaning directory: ${err}\n`,
   uploadDirectoryError: (err) => `\n❌    Error uploading directory: ${err}\n`,
   getDirectoryListError: (err) => `\n❌    Error getting folder's content: ${err}\n`,
